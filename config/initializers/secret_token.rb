a6596b61ee3f24c3d3513f538b7196796dbd3f06
abryazgin <abryazgin@rc-online.org>
2013-10-31 10:46:10 +0600

    создание Hello World-а на Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RorTest::Application.config.secret_key_base = 'd86d0d98a3b700a26e520a41988be72cc5211de46341fcd7e2870b71f40145d4b7f1d3fcf29e1dc1d2f678d2191a33f76e743df854b746fe847e5a792bf0e6f4'
